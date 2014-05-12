51238fbc0a2c0c3315234355cb1dd435860495f0
Akash Patel <akash@akashs-mbp.lan>
2014-05-12 21:31:34 +0200

    Spotify iOS SDK Basic Auth playground

---
+++ SpotifySDKApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpotifySDKApp
+//
+//  Created by Akash Patel on 12/05/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpotifySDKApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpotifySDKApp
+//
+//  Created by Akash Patel on 12/05/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
